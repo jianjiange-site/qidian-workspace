f0685d91008c9072f2b18d2f4992ed9fb8ae7d8b
肖祖德 <2827815597@qq.com>
2026-07-06 08:44:11 +0800

    1

--- dating-server/post-service/model/__init__.py
+++
@@ -1,7 +0,0 @@
-from .post import Post
-from .post_image import PostImage
-from .post_stat import PostStat
-from .post_like import PostLike
-from .post_comment import PostComment
-
-__all__ = ["Post", "PostImage", "PostStat", "PostLike", "PostComment"]
